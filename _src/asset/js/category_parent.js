63e01ec7e2a3ab0a4c3dea4dada0b16ee6ead33f
Jun Tamagawa <jun@rfs.jp>
2020-04-27 21:52:03 +0900

    目次ページ追加

--- _src/asset/js/category_parent.js
+++ _src/asset/js/category_parent.js
@@ -1,4 +1,4 @@
-//@prepros-prepend top.js
+//@prepros-prepend common.js
 //@prepros-prepend swiper.min.js
 
 /*#########################################################
@@ -8,6 +8,41 @@
 #########################################################*/
 document.addEventListener("DOMContentLoaded", function(event) {
 
-
+	/* ========================================================
+	メインビジュアルスライダー
+	=========================================================*/
+	var swiper = new Swiper('.swiper-container', {
+		// effect: 'fade',
+		// init: false,
+		// fadeEffect: {
+		// 	crossFade: true
+		// },
+		autoplay: {
+			delay: 6000
+		},
+		speed: 1500,
+		loop: true,
+		resizeReInit: true,
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+		// preloadImages: false,
+		// lazy: {
+		// 	loadPrevNext: true
+		// },
+		slidesPerView: "auto",
+		centeredSlides: true,
+		spaceBetween: 100,
+		// observer: true,
+		// observeParents: true
+	});
+	// swiper.on('init', function () {
+	// 	document.querySelector(".mv01").classList.add("active");
+	// });
+	// swiper.on('slideChange', function () {
+	// 	document.querySelector(".mv02").classList.add("active");
+	// });
+	// swiper.init();
 });
 
